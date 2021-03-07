feb431ea1f39017ce8b6c46faca8020b100c6e32
Louis Amoah-nuamah <louisan42@gmail.com>
2021-03-07 18:23:07 -0500

    added destination objects

--- portfolio_script.js
+++ portfolio_script.js
@@ -1,8 +1,34 @@
-// Nelson Mandela quotes with date
+// Random vacation lottery
 // Author: Louis Amoah-Nuamah
 
-const randomChoice = () => {
-    return Math.floor(Math.random() * 5)
+const randomChoice = (n) => {
+    return Math.floor(Math.random() * n)
 };
+// uncomment code below to test random pick
+//console.log(randomChoice());
 
-console.log(randomChoice());
+let destinationArr = [{continent:'Africa',cities:['Accra','Cairo','Johannesburg']}, {continent:'Africa',cities:['London','Berlin','Athens']}, 
+{continent:'Africa',cities:['Miami','Vancouver','Cancun']}, {continent:'Africa',cities:['Quito','Bogota','Rio de Janeiro']}, 
+{continent:'Africa',cities:['Bali','Tokyo','Shanghai']}, {continent:'Africa',cities:['Sydney','Queenstown','Vanua Levu']}];
+
+const outputs = {
+    continent : ['Africa','Europe','North America','South America','Asia','Oceania'],
+    destination: [],
+    set destinations(destinationArr){
+        this.destination = destinationArr;
+    },
+    lengthOfStay : ['2 weeks', '3 weeks', '4 weeks']
+
+}
+
+let finalOutput = [];
+outputs.destinations(destinationArr);
+let spinNumber = randomChoice(outputs.continent.length());
+let continent = outputs.continent[spinNumber];
+
+switch(continent){
+    case 'Africa':
+        {
+
+        }
+}
