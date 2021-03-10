d7465731c8c0d770982688b904ba90b8c92af137
Louis Amoah-nuamah <louisan42@gmail.com>
2021-03-09 21:12:36 -0500

    final working piece

--- portfolio_script.js
+++ portfolio_script.js
@@ -3,7 +3,7 @@
 
 
 
-const randomChoice = (n, addMe) => {
+const randomChoice = (n) => {
     return Math.floor(Math.random() * n)
 };
 // uncomment code below to test random pick
@@ -14,7 +14,7 @@ let destinationArr = [{continent:'Africa',cities:['Accra','Cairo','Johannesburg'
 {continent:'Asia',cities:['Bali','Tokyo','Shanghai']}, {continent:'Oceania',cities:['Sydney','Queenstown','Vanua Levu']}];
 
 const outputs = {
-    travelPackagee: ['couple\'s', 'Group of 4', 'Single\'s nightlife' ],
+    travelPackage: ['couple\'s', 'Group of 4', 'Single\'s nightlife' ],
     destination: [],
     set destinations(destinationArr) {
         this.destination = destinationArr;
@@ -23,9 +23,10 @@ const outputs = {
 
 }
 
+
 let finalOutput = [];
 outputs.destinations = destinationArr;
-// set posibily of not winning by expanding number of choice greater than the lenght of array
+// set posibility of not winning by expanding number of choice greater than lenght of array
 // makes function resuable to access city array within destination objects
 const flocationValue = (objArr, extraLength) => {
   let spinNumber = randomChoice(objArr.length + extraLength);
@@ -48,47 +49,49 @@ if(continentObj === undefined ){
     cityObj = continentObj.cities;
     finalOutput.push('congratulations!!!')
 };
-
-let continentIndex = outputs.destination.indexOf(continentObj);
-
+// randomly pick travel packages and length of stay
+let package = flocationValue(outputs.travelPackage, 0);
+let lengthOfStay = flocationValue(outputs.lengthOfStay, 0);
+// switch based on continent to randomly select city 
 switch(continent){
     case 'Africa':
-        {
-                        
+        {           
             city = flocationValue(cityObj, 0);
             break;
         }
     case 'Europe':
         {
-            
             city = flocationValue(cityObj,0);
             break;
         }
     case 'North America':
         {
-            
             city = flocationValue(cityObj,0);
             break;
         }
     case 'South America':
         {
-            
             city = flocationValue(cityObj,0);
             break;
         }
     case 'Asia':
         {
-            
             city = flocationValue(cityObj,0);
             break;
         }
     case 'Oceania':
         {
-            
             city = flocationValue(cityObj,0);
             break;
         }
     default : 
-       console.log('Better luck next time!');
+       finalOutput.push('Better luck next time!');
 };
-finalOutput.push(`You have won a a trip to ${city} in ${continent}. `)
+// if condition is unflagged, push to message array
+if (continent != 'error') {
+    finalOutput.push(`You have won a trip to ${city} in ${continent}. `)
+    finalOutput.push(`Package for ${package}. `)
+    finalOutput.push(`You will spend ${lengthOfStay} in this destination.`)
+}
+const lotteryOutcome = finalOutput.join('\n');
+console.log(lotteryOutcome);
